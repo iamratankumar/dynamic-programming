5970ac2a20e1848c03815b3679af78a2c7d2786e
iamratankumar <iamratankumar@yahoo.com>
2025-05-26 14:58:06 -0500

    Added Fibonacci solved using DP

---
+++ DynamicProgramming/Fibonacci.cs
@@ -0,0 +1,53 @@
+﻿namespace DynamicProgramming;
+
+public static class Fibonacci
+{
+    //Exponential runtime and n space time
+    // Not suitable for larger numbers
+    public static int ComputeBruteForce(int n)
+    {
+        if (n < 2) return n;
+
+        return ComputeBruteForce(n - 1) + ComputeBruteForce(n -2);
+    }
+    
+    
+    //~n runtime and n space
+    //better performance
+    //top bottom with cache
+
+    public static int ComputeMemoized(int n)
+    {
+        return ComputeMemoized(n, new Dictionary<int, int>());
+    }
+
+    private static int ComputeMemoized(int n, Dictionary<int, int> cache)
+    {
+        if (cache.TryGetValue(n,out var value)) return cache[n];
+        if (n < 2) return n;
+
+        var fib = ComputeMemoized(n - 1, cache) + ComputeMemoized(n - 2, cache);
+        cache[n] = fib;
+        return fib;
+    }
+    
+    
+    //n runtime and space
+    //Bottom up approch
+    //efficient
+
+    public static int ComputeBottomUp(int n)
+    {
+        var fibs = new int[n + 1];
+        fibs[1] = 1;
+
+        for (var i = 2; i <= n; i++)
+        {
+            fibs[i] = fibs[i - 1] + fibs[i - 2];
+        }
+
+        return fibs[n];
+    }
+
+
+}
---
+++ DynamicProgramming/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DynamicProgramming;
+
+Console.WriteLine(Fibonacci.ComputeBottomUp(111110));
